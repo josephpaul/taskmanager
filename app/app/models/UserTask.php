8c2886d83b494f4c1d6a38041bacb95705882a46
josephpaul <josephpaul213@gmail.com>
2014-10-31 20:56:06 +0530

    User task complete

--- app/app/models/UserTask.php
+++ app/app/models/UserTask.php
@@ -2,7 +2,16 @@
 
 class UserTask extends Eloquent
 {
+	protected $table="user_task";
 
+	public function user()
+	{
+	return $this->belongsTo("User", "user_id", "id");
+	}
+	public function task()
+	{
+		return $this->belongsTo("Task", "id", "task_id");
+	}
 }
 
 ?>
