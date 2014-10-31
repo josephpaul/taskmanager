3d49a0b68779b3861469415fa033b898df867d16
josephpaul <josephpaul213@gmail.com>
2014-10-31 20:50:17 +0530

    Org complete

--- app/app/models/Org.php
+++ app/app/models/Org.php
@@ -3,7 +3,7 @@ class Org extends Eloquent
 {
 	protected $table="org_details";
 
-  protected $fillable=["name","admin"];
+	protected $fillable=["name","admin"];
 
 	static $rule=["name"=>"required|min:3|max:30"];
 
@@ -11,6 +11,15 @@ class Org extends Eloquent
 	{
 		$validate=Validator::make($data,static::$rule);
 		return $validate;
+	}
+
+	public function projects()
+	{
+	return $this->hasMany("Projects", "org_id", "id");
+	}
+	public function orguser()
+	{
+		return $this->hasMany("OrgUser", "org_id", "id");
 	}
 	
 }
