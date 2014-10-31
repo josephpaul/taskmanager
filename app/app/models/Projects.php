17767150864e95ebecb6cba4f94d2edef21c6378
josephpaul <josephpaul213@gmail.com>
2014-10-31 20:52:18 +0530

    Pro model complete

--- app/app/models/Projects.php
+++ app/app/models/Projects.php
@@ -5,7 +5,23 @@ class Projects extends Eloquent
 
 	protected $fillable=["name","admin","org_id"];
 
+	static $rule=["name"=>"required|min:3|max:50"];
 
+	public static function validate($data)
+	{
+		$validate=Validator::make($data,static::$rule);
+		return $validate;
+	}
+
+	public function org()
+	{
+		return $this->belongsTo('Org', 'org_id', 'id');
+	}
+     
+     public function task()
+     {
+     	return $this->hasMany('Task', 'pro_id', 'id');
+     }
 
 }
 ?>
