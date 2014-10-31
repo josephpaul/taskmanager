2ccd4f81ab267958ee3eebd5418bbed6145dfc6a
josephpaul <josephpaul213@gmail.com>
2014-10-31 20:53:56 +0530

    Task extra

--- app/app/models/Task.php
+++ app/app/models/Task.php
@@ -3,6 +3,12 @@ class Task extends Eloquent
 {
 	protected $table="task_details";
 
+	protected $fillable=["title","des","admin","org_id","pro_id", "assign_to","date","time"];
+
+	static $rule=[
+	"title"=>"required|min:3|max:50",
+	"des"=>"required|min:5|max:500",
+	"assin"=>"required"];
 
 
 }
