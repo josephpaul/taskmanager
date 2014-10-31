132eff8fcecdce883b18203582861d2d9445fe0c
josephpaul <josephpaul213@gmail.com>
2014-10-31 20:50:05 +0530

    setup rule

--- app/app/models/Org.php
+++ app/app/models/Org.php
@@ -3,7 +3,15 @@ class Org extends Eloquent
 {
 	protected $table="org_details";
 
+  protected $fillable=["name","admin"];
 
+	static $rule=["name"=>"required|min:3|max:30"];
+
+	public static function validate($data)
+	{
+		$validate=Validator::make($data,static::$rule);
+		return $validate;
+	}
 	
 }
 
