ceb2645d0082030c1446f62483dbb8afd6de1f7b
josephpaul <josephpaul213@gmail.com>
2014-10-31 20:53:07 +0530

    ProUser complete

--- app/app/models/ProUser.php
+++ app/app/models/ProUser.php
@@ -5,6 +5,19 @@ class ProUser extends Eloquent
 
 	protected $fillable=["user_id","pro_id"];
     
+    static $rule=["user_email"=>"required|email|exists:user_details,email"];
+
+    public static function validate($data)
+	{
+		$validate=Validator::make($data,static::$rule);
+		return $validate;
+	}
+
+    public function user()
+    {
+    	return $this->belongsTo("User","user_id", "id");
+    }
+
 
 
 
